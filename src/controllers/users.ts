3fcf096cb31139ddd61fdccbf27bc287ab13ff9a
unknown <muizzuddin334@gmail.com>
2024-07-08 12:27:43 +0700

    code transaksi response

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -14,15 +14,21 @@ class UsersControl {
       await UserValidation.add(body);
 
       const tanggal: string = format(new Date(2014, 1, 11), "ddMMyyyy");
+      const codeTransaksi = Math.floor(
+        10000 + Math.random() * 90000
+      ).toString();
+
       body.codeTransaksi =
-        codeCrypto[body.cryptoLoan as CrytoLoan] +
-        tanggal +
-        Math.floor(10000 + Math.random() * 90000).toString();
+        codeCrypto[body.cryptoLoan as CrytoLoan] + tanggal + codeTransaksi;
 
       const user = new UsersCol(body);
       const insertedID = (await user.save())._id;
 
-      res.status(201).json({ message: "Successfully added user", insertedID });
+      res.status(201).json({
+        message: "Successfully added user",
+        insertedID,
+        codeTransaksi: codeTransaksi,
+      });
       return;
     } catch (err) {
       next(err);
